00afd4255ae8c245fb179c5adbe36ec7654666fc
LordRonz <aarontanhar2000@gmail.com>
2022-01-28 09:03:24 +0700

    feat: initialize layout

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,5 +1,14 @@
+import Footer from '@/components/layout/Footer';
+import Header from '@/components/layout/Header';
+
 const Layout = ({ children }: { children: React.ReactNode }) => {
-  return <>{children}</>;
+  return (
+    <>
+      <Header />
+      {children}
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
