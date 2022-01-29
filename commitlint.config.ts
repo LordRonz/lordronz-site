2eb180e8a1c381f71b9edec717dd0cf337e756f7
LordRonz <aarontanhar2000@gmail.com>
2022-01-29 21:36:05 +0700

    feat: change commitlint config to ts

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,4 +1,6 @@
-module.exports = {
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     // TODO Add Scope Enum Here
@@ -23,3 +25,5 @@ module.exports = {
     ],
   },
 };
+
+export default Configuration;
