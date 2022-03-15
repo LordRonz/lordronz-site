4d01c76024e568f269509ca0de0ced9026478141
LordRonz <aarontanhar2000@gmail.com>
2022-03-15 11:37:40 +0700

    feat: use nextjs-progressbar :rocket:

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,19 @@
 import '@/styles/globals.css';
 import 'react-tippy/dist/tippy.css';
-import '@/styles/nprogress.css';
 import 'react-typed/dist/animatedCursor.css';
 
 import type { AppProps } from 'next/app';
-import Router from 'next/router';
 import { ThemeProvider } from 'next-themes';
-import nProgress from 'nprogress';
-
-Router.events.on('routeChangeStart', nProgress.start);
-Router.events.on('routeChangeError', nProgress.done);
-Router.events.on('routeChangeComplete', nProgress.done);
+import NextNProgress from 'nextjs-progressbar';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false}>
+      <NextNProgress
+        color='#eb2754'
+        startPosition={0.2}
+        options={{ showSpinner: false }}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   );
