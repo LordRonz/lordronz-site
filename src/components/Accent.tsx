69123b555c6dbc4fa583aafdc2ed6324e39798da
LordRonz <aarontanhar2000@gmail.com>
2022-05-14 11:38:36 +0700

    feat: change accent light mode to be more contrast :rocket:

--- src/components/Accent.tsx
+++ src/components/Accent.tsx
@@ -8,7 +8,7 @@ const Accent = ({ children, className }: AccentType) => {
       className={clsxm(
         'transition-colors',
         // 'from-primary-300/40 to-primary-400/40 via-primary-300/40',
-        'bg-gradient-to-tr from-primary-300 via-primary-100/70 to-primary-400 bg-clip-text text-transparent dark:from-primary-300 dark:via-primary-100/70 dark:to-sky-300',
+        'bg-gradient-to-tr from-primary-300 via-rose-700/70 to-primary-400 bg-clip-text text-transparent dark:from-primary-300 dark:via-primary-100/70 dark:to-sky-300',
         className
       )}
     >
