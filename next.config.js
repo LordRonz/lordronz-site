07b10839d3221a736219e610223c30ed8347d109
LordRonz <aarontanhar2000@gmail.com>
2022-07-07 11:03:13 +0700

    feat: preact is back yo :rocket:

--- next.config.js
+++ next.config.js
@@ -17,13 +17,13 @@ const nextConfig = {
   reactStrictMode: true,
   webpack: (config, { dev, isServer }) => {
     // Replace React with Preact only in client production build
-    // if (!dev && !isServer) {
-    //   Object.assign(config.resolve.alias, {
-    //     react: 'preact/compat',
-    //     'react-dom/test-utils': 'preact/test-utils',
-    //     'react-dom': 'preact/compat',
-    //   });
-    // }
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: 'preact/compat',
+        'react-dom/test-utils': 'preact/test-utils',
+        'react-dom': 'preact/compat',
+      });
+    }
 
     config.module.rules.push({
       test: /\.svg$/i,
