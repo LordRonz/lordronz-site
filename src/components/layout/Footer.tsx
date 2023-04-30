fec616d83550d49807e932d1711dd3d2a9b275e0
LordRonz <aarontanhar2000@gmail.com>
2023-04-30 09:59:57 +0700

    fix: mail tooltip spacing :rocket:

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -71,7 +71,7 @@ const Footer = () => {
               html={
                 <div className='inline-block rounded-md border bg-white p-2 text-gray-600 shadow-md dark:border-primary-500 dark:bg-dark dark:text-gray-200'>
                   {copyStatus}
-                  <Accent className='inline-block font-medium'>{mail}</Accent>
+                  <Accent className='inline-block font-medium'> {mail}</Accent>
                 </div>
               }
             >
