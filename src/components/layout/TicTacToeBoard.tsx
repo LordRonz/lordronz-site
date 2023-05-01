812efefb3f31e9cae6ab64136e306b9cef0b0614
LordRonz <aarontanhar2000@gmail.com>
2023-05-02 00:29:37 +0700

    feat: ttt responsive :rocket:

--- src/components/layout/TicTacToeBoard.tsx
+++ src/components/layout/TicTacToeBoard.tsx
@@ -15,12 +15,12 @@ const TicTacToeBoard = ({
   ) => void;
 }) => {
   return (
-    <section className='mx-auto grid h-[454px] w-[454px] grid-cols-3 border-2 border-gray-500'>
+    <section className='mx-auto grid h-[304px] w-[304px] grid-cols-3 border-2 border-gray-500 md:h-[454px] md:w-[454px]'>
       {data.board.map((row, i) =>
         row.map((col, j) => (
           <div
             className={clsxm(
-              'flex h-[150px] w-[150px] cursor-pointer select-none items-center justify-center border border-gray-500 bg-slate-300 text-8xl transition-colors hover:bg-primary-50 dark:bg-gray-600',
+              'flex h-[100px] w-[100px] cursor-pointer select-none items-center justify-center border border-gray-500 bg-slate-300 text-7xl transition-colors hover:bg-primary-50 dark:bg-gray-600 md:h-[150px] md:w-[150px] md:text-8xl',
               col === 'X' && 'text-red-500',
               col === 'O' && 'text-green-500'
             )}
