e7e1c49e17d5a972222940bff809f4db7296ebce
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2023-09-17 14:56:22 +0700

    feat: three js render

---
+++ src/components/layout/Graphic.tsx
@@ -0,0 +1,82 @@
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useFrame } from '@react-three/fiber';
+import { useRef } from 'react';
+import type {
+  BufferGeometry,
+  Group,
+  Material,
+  NormalBufferAttributes,
+  Object3DEventMap,
+  Points,
+} from 'three';
+
+export const PixelatedSphere = () => {
+  const sphereRef =
+    useRef<
+      Points<BufferGeometry<NormalBufferAttributes>, Material | Material[]>
+    >(null);
+
+  useFrame(({ clock }) => {
+    if (sphereRef.current) {
+      sphereRef.current.rotation.y = clock.getElapsedTime() * 1.2;
+      sphereRef.current.rotation.z = clock.getElapsedTime() * 0.7;
+    }
+  });
+
+  return (
+    <points ref={sphereRef}>
+      <icosahedronGeometry args={[1, 4]} />
+      <pointsMaterial color='#eb2754' size={0.05} />
+    </points>
+  );
+};
+
+export const Birds = () => {
+  const groupRef = useRef<Group<Object3DEventMap>>(null);
+
+  useFrame(({ clock }) => {
+    if (!groupRef.current) return;
+    const elapsedTime = clock.getElapsedTime();
+    groupRef.current.children.forEach(
+      (
+        bird: { position: { x: number; y: number; z: number } },
+        index: number
+      ) => {
+        const angle = (elapsedTime + index * 0.2) % (2 * Math.PI);
+        const radius = 2.3 + Math.random() * 0.0001;
+        bird.position.x =
+          radius * Math.cos(angle) - Math.sin(elapsedTime * 0.5 + index);
+        bird.position.y =
+          radius * Math.sin(angle) * Math.sin(elapsedTime * 0.5 + index);
+        bird.position.z = radius * Math.cos(elapsedTime * 0.5 + index);
+      }
+    );
+  });
+
+  const birds = [...Array(36)].map((_, i) => {
+    const size = i % 2 === 0 ? 0.03 : 0.05;
+    return (
+      <mesh key={i} position={[1, 0, 0]}>
+        <planeGeometry args={[size, size]} />
+        <meshBasicMaterial color='#eb2754' />
+      </mesh>
+    );
+  });
+
+  return <group ref={groupRef}>{birds}</group>;
+};
+
+export const Graphic = () => {
+  return (
+    <div>
+      <Canvas
+        camera={{ fov: 40, position: [0, 0, 5] }}
+        gl={{ antialias: false }}
+      >
+        <PixelatedSphere />
+        <Birds />
+        <OrbitControls enableZoom={false} />
+      </Canvas>
+    </div>
+  );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import type { NextPage } from 'next';
 import Accent from '@/components/Accent';
 import AnimatePage from '@/components/AnimatePage';
 import CoolButton from '@/components/buttons/CoolButton';
+import { Graphic } from '@/components/layout/Graphic';
 import Layout from '@/components/layout/Layout';
 import CustomLink from '@/components/links/CustomLink';
 import UnstyledLink from '@/components/links/UnstyledLink';
@@ -16,47 +17,54 @@ const Home: NextPage = () => {
       <Seo />
       <AnimatePage>
         <main>
-          <section className={clsxm('flex flex-col justify-center')}>
+          <section
+            className={clsxm('flex flex-row items-center justify-center')}
+          >
             <article className='layout'>
-              <h1 className='text-4xl font-bold md:text-6xl 2xl:text-7xl'>
-                <Accent>Henlo there </Accent>
-                <Wave>👋</Wave>
-              </h1>
-              <p
-                className={clsxm(
-                  'my-4 max-w-4xl md:mt-6',
-                  'md:text-lg 2xl:text-xl'
-                )}
-              >
-                I am a passionate Programmer, and love tinkering with Python to
-                automate my personal tasks. I have a good experience with
-                backend technologies and SysAdmin. I also have developed
-                frontend skills by making some React projects.
-              </p>
-              <UnstyledLink
-                href='/about'
-                aria-label='About Page'
-                className='inline-flex'
-              >
-                <CoolButton
-                  wrapperClassName='text-md font-bold'
-                  width={210}
-                  height={45}
-                >
-                  Learn More About Me
-                </CoolButton>
-              </UnstyledLink>
-              <p className='mt-8'>
-                P.S. this website is open-source and available on{' '}
-                <CustomLink
-                  href='https://github.com/lordronz/lordronz-site'
-                  title='Link to GitHub repository'
-                  aria-label='Link to GitHub repository'
-                >
-                  GitHub
-                </CustomLink>
-                .
-              </p>
+              <div className='flex h-full flex-col items-center gap-x-4 gap-y-4 md:flex-row'>
+                <div className='flex flex-col'>
+                  <h1 className='text-4xl font-bold md:text-6xl 2xl:text-7xl'>
+                    <Accent>Henlo there </Accent>
+                    <Wave>👋</Wave>
+                  </h1>
+                  <p
+                    className={clsxm(
+                      'my-4 max-w-4xl md:mt-6',
+                      'md:text-lg 2xl:text-xl'
+                    )}
+                  >
+                    I am a passionate Programmer, and love tinkering with Python
+                    to automate my personal tasks. I have a good experience with
+                    backend technologies and SysAdmin. I also have developed
+                    frontend skills by making some React projects.
+                  </p>
+                  <UnstyledLink
+                    href='/about'
+                    aria-label='About Page'
+                    className='inline-flex'
+                  >
+                    <CoolButton
+                      wrapperClassName='text-md font-bold'
+                      width={210}
+                      height={45}
+                    >
+                      Learn More About Me
+                    </CoolButton>
+                  </UnstyledLink>
+                  <p className='mt-8'>
+                    P.S. this website is open-source and available on{' '}
+                    <CustomLink
+                      href='https://github.com/lordronz/lordronz-site'
+                      title='Link to GitHub repository'
+                      aria-label='Link to GitHub repository'
+                    >
+                      GitHub
+                    </CustomLink>
+                    .
+                  </p>
+                </div>
+                <Graphic />
+              </div>
             </article>
           </section>
         </main>
