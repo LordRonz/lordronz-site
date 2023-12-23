d0bddc0dab52fdeaaf70ef2d9cf8ad24be2f9629
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2023-12-24 01:29:47 +0700

    fix: sonarcloud duplicate code

---
+++ src/components/content/SingleProductIndexPage.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+import Lightbox from 'react-image-lightbox';
+
+import Accent from '@/components/Accent';
+import Layout from '@/components/layout/Layout';
+import ProjectCard, { Project } from '@/components/ProjectCard';
+import Seo from '@/components/Seo';
+import clsxm from '@/lib/clsxm';
+
+const SingleProductIndexPage = ({ products }: { products: Project[] }) => {
+  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
+  const [imgIndex, setImgIndex] = useState(0);
+
+  return (
+    <Layout>
+      <Seo templateTitle='Products' description='List of my products' />
+      <main>
+        <section className={clsxm('flex flex-col justify-center')}>
+          <article className='layout space-y-10 py-8'>
+            <h1 className='text-3xl md:text-5xl 2xl:text-6xl'>
+              <Accent>Products</Accent>
+            </h1>
+            <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
+              {products.map((project) => (
+                <ProjectCard
+                  project={project}
+                  key={project.slug}
+                  onImgClick={() => {
+                    if (project.i === undefined) return;
+                    setImgIndex(project.i);
+                    setIsLightboxOpen(true);
+                  }}
+                  imageLinkPrefix='product'
+                />
+              ))}
+            </div>
+          </article>
+        </section>
+        {isLightboxOpen && (
+          <Lightbox
+            mainSrc={`/images/product/${products[imgIndex].image}`}
+            onCloseRequest={() => setIsLightboxOpen(false)}
+          />
+        )}
+      </main>
+    </Layout>
+  );
+};
+
+export default SingleProductIndexPage;
--- src/pages/product/spigger/index.tsx
+++ src/pages/product/spigger/index.tsx
@@ -1,19 +1,14 @@
 import type { NextPage } from 'next';
-import { useState } from 'react';
-import Lightbox from 'react-image-lightbox';
 
-import Accent from '@/components/Accent';
-import Layout from '@/components/layout/Layout';
-import ProjectCard, { Project } from '@/components/ProjectCard';
-import Seo from '@/components/Seo';
-import clsxm from '@/lib/clsxm';
+import SingleProductIndexPage from '@/components/content/SingleProductIndexPage';
+import { Project } from '@/components/ProjectCard';
 
 const products: Project[] = [
   {
     title: 'Spigger',
     content:
       'Risaikuru is a web service that can classify your trash to determine if it is organic or reusable',
-    slug: 'number-ninja',
+    slug: 'spigger',
     type: 'app',
     github: 'LordRonz/spigger',
     demo: 'https://risaikuru.vercel.app/',
@@ -22,42 +17,7 @@ const products: Project[] = [
 ].map((project, i) => ({ ...project, i }));
 
 const Product: NextPage = () => {
-  const [isLightboxOpen, setIsLightboxOpen] = useState(false);
-  const [imgIndex, setImgIndex] = useState(0);
-
-  return (
-    <Layout>
-      <Seo templateTitle='Projects' description='List of my projects' />
-      <main>
-        <section className={clsxm('flex flex-col justify-center')}>
-          <article className='layout space-y-10 py-8'>
-            <h1 className='text-3xl md:text-5xl 2xl:text-6xl'>
-              <Accent>Projects</Accent>
-            </h1>
-            <div className='grid grid-cols-1 gap-4 md:grid-cols-2'>
-              {products.map((project) => (
-                <ProjectCard
-                  project={project}
-                  key={project.slug}
-                  onImgClick={() => {
-                    if (project.i === undefined) return;
-                    setImgIndex(project.i);
-                    setIsLightboxOpen(true);
-                  }}
-                />
-              ))}
-            </div>
-          </article>
-        </section>
-        {isLightboxOpen && (
-          <Lightbox
-            mainSrc={`/images/product/${products[imgIndex].image}`}
-            onCloseRequest={() => setIsLightboxOpen(false)}
-          />
-        )}
-      </main>
-    </Layout>
-  );
+  return <SingleProductIndexPage products={products} />;
 };
 
 export default Product;
