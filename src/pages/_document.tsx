58eb0e2f175f512f4c7c4b8c6a1d657ebb54d87f
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-04-28 00:58:39 +0700

    feat: add umami analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,7 +15,14 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang='en' className='scroll-smooth'>
-        <Head />
+        <Head>
+          <script
+            async
+            defer
+            src='https://analytics.us.umami.is/script.js'
+            data-website-id='51739fff-d062-4217-9533-180ec6523428'
+          />
+        </Head>
         <body className='bg-light tracking-wide text-dark transition-all duration-300 selection:bg-[rgb(var(--tw-clr-primary-300)_/_30%)] dark:bg-dark dark:text-light'>
           <Main />
           <NextScript />
