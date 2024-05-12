6915944c2a2cd571ac10aac0f547eb170c6a49ac
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-05-12 14:03:19 +0700

    feat: manifest

---
+++ src/app/manifest.ts
@@ -0,0 +1,52 @@
+import { MetadataRoute } from 'next';
+
+import { MY_NAME } from '@/constants/metadata';
+
+const manifest = (): MetadataRoute.Manifest => {
+  return {
+    name: MY_NAME,
+    short_name: MY_NAME.split(' ')[0],
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#111',
+    theme_color: '#eb2754',
+    lang: 'en',
+    categories: ['Technology', 'Programming', 'Website'],
+    description: "Aaron christopher's online portfolio and blog website.",
+    orientation: 'portrait-primary',
+    icons: [
+      {
+        src: '/favicon/android-icon-36x36.png',
+        sizes: '36x36',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-icon-48x48.png',
+        sizes: '48x48',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-icon-72x72.png',
+        sizes: '72x72',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-icon-96x96.png',
+        sizes: '96x96',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-icon-144x144.png',
+        sizes: '144x144',
+        type: 'image/png',
+      },
+      {
+        src: '/favicon/android-icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+    ],
+  };
+};
+
+export default manifest;
--- src/scripts/pre-build/createManifest.ts
+++ src/scripts/pre-build/createManifest.ts
@@ -2,52 +2,50 @@ import { existsSync } from 'fs';
 import fs from 'fs/promises';
 import path from 'path';
 
+import { MY_NAME } from '@/constants/metadata';
 import { IScriptParams } from '@/scripts/runner';
 
 const manifest = {
-  name: 'LordRonz Site',
-  short_name: 'Ronz Site',
+  name: MY_NAME,
+  short_name: MY_NAME.split(' ')[0],
   start_url: '/',
   display: 'standalone',
   background_color: '#111',
   theme_color: '#eb2754',
+  lang: 'en',
+  categories: ['Technology', 'Programming', 'Website'],
+  description: "Aaron christopher's online portfolio and blog website.",
   orientation: 'portrait-primary',
   icons: [
     {
       src: '/favicon/android-icon-36x36.png',
       sizes: '36x36',
       type: 'image/png',
-      density: '0.75',
     },
     {
       src: '/favicon/android-icon-48x48.png',
       sizes: '48x48',
       type: 'image/png',
-      density: '1.0',
     },
     {
       src: '/favicon/android-icon-72x72.png',
       sizes: '72x72',
       type: 'image/png',
-      density: '1.5',
     },
     {
       src: '/favicon/android-icon-96x96.png',
       sizes: '96x96',
       type: 'image/png',
-      density: '2.0',
     },
     {
       src: '/favicon/android-icon-144x144.png',
       sizes: '144x144',
       type: 'image/png',
-      density: '3.0',
     },
     {
       src: '/favicon/android-icon-192x192.png',
       sizes: '192x192',
       type: 'image/png',
-      density: '4.0',
     },
   ],
 };
