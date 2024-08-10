567d8cd8fd0fb6e09bdb8ec93ffd01de6da6b89a
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-08-10 19:33:39 +0700

    fix: custom code clipboard

--- src/components/content/CustomCode.tsx
+++ src/components/content/CustomCode.tsx
@@ -42,31 +42,33 @@ const CustomCode = (
       )}
 
       {!!language && (
-        <div className='absolute left-6 top-0 rounded-b-md border border-t-0 border-gray-600 px-2 py-1'>
-          <span className='select-none bg-gradient-to-tr from-primary-300 to-primary-400 bg-clip-text font-medium text-transparent'>
-            {language}
-          </span>
-        </div>
-      )}
-      <CopyToClipboard
-        text={textRef?.current?.textContent ?? ''}
-        onCopy={() => {
-          setIsCopied(true);
-          setTimeout(() => setIsCopied(false), 1500);
-        }}
-      >
-        <button className='absolute md:right-2 md:top-2 right-1 top-1 block rounded border border-gray-600 p-2 text-lg transition-colors hover:bg-gray-400 hover:dark:bg-gray-700 md:block'>
-          <label
-            className={clsxm(
-              'swap swap-rotate grid',
-              isCopied && 'swap-active',
-            )}
+        <>
+          <div className='absolute left-6 top-0 rounded-b-md border border-t-0 border-gray-600 px-2 py-1'>
+            <span className='select-none bg-gradient-to-tr from-primary-300 to-primary-400 bg-clip-text font-medium text-transparent'>
+              {language}
+            </span>
+          </div>
+          <CopyToClipboard
+            text={textRef?.current?.textContent ?? ''}
+            onCopy={() => {
+              setIsCopied(true);
+              setTimeout(() => setIsCopied(false), 1500);
+            }}
           >
-            <FiCheck className='text-primary-400 swap-on' />
-            <FiCopy className='swap-off' />
-          </label>
-        </button>
-      </CopyToClipboard>
+            <button className='absolute md:right-2 md:top-2 right-1 top-1 block rounded border border-gray-600 p-2 text-lg transition-colors hover:bg-gray-400 hover:dark:bg-gray-700 md:block'>
+              <label
+                className={clsxm(
+                  'swap swap-rotate grid',
+                  isCopied && 'swap-active',
+                )}
+              >
+                <FiCheck className='text-primary-400 swap-on' />
+                <FiCopy className='swap-off' />
+              </label>
+            </button>
+          </CopyToClipboard>
+        </>
+      )}
     </code>
   );
 };
