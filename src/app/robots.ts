eecd9050297a23378c66f44709568c2f6b8f07a7
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-11-05 09:58:50 +0700

    fix: invalid robots.txt

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -1,12 +1,14 @@
 import type { MetadataRoute } from 'next';
 
+import { WEBSITE_URL } from '@/constants/env';
+
 const robots = (): MetadataRoute.Robots => {
   return {
     rules: {
       userAgent: '*',
       allow: '/',
     },
-    sitemap: './sitemap.xml',
+    sitemap: `${WEBSITE_URL}/sitemap.xml`,
   };
 };
 
