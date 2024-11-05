b2340cbaafcd5cd3161081a132a4295bc7322646
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-11-05 10:51:08 +0700

    feat: jsonld use from const

--- src/components/metadata/JsonLd.tsx
+++ src/components/metadata/JsonLd.tsx
@@ -1,6 +1,7 @@
 import type { Thing, WebSite, WithContext } from 'schema-dts';
 
 import { WEBSITE_URL } from '@/constants/env';
+import { MY_NAME } from '@/constants/metadata';
 
 export type JsonLdProps = {
   children: React.ReactNode;
@@ -10,7 +11,7 @@ export type JsonLdProps = {
 export const defaultJsonLd: WithContext<WebSite> = {
   '@context': 'https://schema.org',
   '@type': 'WebSite',
-  name: 'Aaron Christopher Tanhar',
+  name: MY_NAME,
   sameAs: [
     'https://www.linkedin.com/in/aaronchristopher69',
     'https://github.com/lordronz',
@@ -20,7 +21,7 @@ export const defaultJsonLd: WithContext<WebSite> = {
     'A software engineer with a passion for building scalable, efficient, and user-centric solutions. I specialize in full-stack development, combining expertise in modern frameworks and technologies to create impactful applications. With a focus on clean code and innovative problem-solving, my portfolio showcases projects across various industries, from web applications to mobile solutions. Explore my blog for insights on development best practices, tech trends, and coding tips.',
   publisher: {
     '@type': 'Person',
-    name: 'Aaron Christopher',
+    name: MY_NAME,
   },
 };
 
