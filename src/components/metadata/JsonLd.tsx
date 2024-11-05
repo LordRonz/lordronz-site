a1bcec4c969b350743a0705b7f4f0e9313bbced3
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-11-05 10:47:27 +0700

    feat: update json ld

--- src/components/metadata/JsonLd.tsx
+++ src/components/metadata/JsonLd.tsx
@@ -11,6 +11,10 @@ export const defaultJsonLd: WithContext<WebSite> = {
   '@context': 'https://schema.org',
   '@type': 'WebSite',
   name: 'Aaron Christopher Tanhar',
+  sameAs: [
+    'https://www.linkedin.com/in/aaronchristopher69',
+    'https://github.com/lordronz',
+  ],
   url: WEBSITE_URL,
   description:
     'A software engineer with a passion for building scalable, efficient, and user-centric solutions. I specialize in full-stack development, combining expertise in modern frameworks and technologies to create impactful applications. With a focus on clean code and innovative problem-solving, my portfolio showcases projects across various industries, from web applications to mobile solutions. Explore my blog for insights on development best practices, tech trends, and coding tips.',
