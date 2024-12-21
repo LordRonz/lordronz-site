072ca19e203692e926fddcc95e205b2c7b37dcfc
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2024-12-21 12:02:28 +0700

    fix: revert postcss

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,3 +1,19 @@
-import postcssConfig from './postcss.config';
+const postcssConfig = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+    'postcss-flexbugs-fixes': {},
+    'postcss-preset-env': {
+      autoprefixer: {
+        flexbox: 'no-2009',
+      },
+      stage: 3,
+      features: {
+        'custom-properties': false,
+      },
+    },
+    ...(process.env.NODE_ENV === 'production' && { cssnano: {} }),
+  },
+};
 
 export default postcssConfig;
