35ec0cc32756b35e33d059359832159365808a2f
Aaron Christopher Tanhar <aaron.christopher@komunal.co.id>
2025-08-10 14:56:29 +0700

    chore: update Playwright configuration and install dependencies

--- playwright.config.ts
+++ playwright.config.ts
@@ -30,6 +30,8 @@ export default defineConfig({
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    baseURL: 'http://localhost:3000',
+    headless: true,
   },
 
   /* Configure projects for major browsers */
@@ -71,9 +73,9 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+    command: 'pnpm run start',
+    url: 'http://localhost:3000',
+    reuseExistingServer: !process.env.CI,
+  },
 });
